fa0c6d70ff033bff30b1d9e6d00b6cf76608a1f0
DaejonFerrao <daejonferrao@gmail.com>
2024-01-17 23:04:06 +0000

    added a background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Header from "./Components/Header/Header";
+import NavBar from './Components/Navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
-  
+      <NavBar />
     </div>
   );
 }
---
+++ src/Components/Navbar/NavBar.js
@@ -0,0 +1,22 @@
+import "./NavBar.css"
+
+// import sunny from "../../Assets/sunny.png";
+
+const NavBar = () => {
+    return (
+        <div>
+            <div className="bg-img">
+                <div className="container">
+                    <div className="topnav">
+                        <a href="Home">Home</a>
+                        <a href="News">News</a>
+                        <a href="Contact">Contact</a>
+                        <a href="About">About</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
