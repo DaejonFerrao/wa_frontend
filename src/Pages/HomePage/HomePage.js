d1a66832d44b36a4e574cb59d11dc007aea13d38
DaejonFerrao <daejonferrao@gmail.com>
2024-01-18 12:55:43 +0000

    added more stats

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,23 +1,29 @@
 import { useEffect , useState} from "react"
+import { currentWeater } from "../../WeatherService"
 
 export const HomePage = () => {
 const [currentWeatherState, setCurrentWeatherState] = useState({})
     useEffect(()=>{
 
         const handleCurrentWeather = async ()=>{
-            const weatherData = await currentWeather()
+            const weatherData = await currentWeater()
+             
             setCurrentWeatherState(weatherData)
 
 
         }
-        handleCurrentWeather
+        handleCurrentWeather()
 
-    })
+    },[])
   return (
     <div>
         <h1>Home page</h1>
         <h2>{currentWeatherState.temp}</h2>
-      
+        <h3>{currentWeatherState.humidity}</h3>
+        <h4>{currentWeatherState.speed}</h4>
+        <h5>{currentWeatherState.country}</h5>
+        <h6>{currentWeatherState.temp_min}</h6>
+        <h7>{currentWeatherState.name}</h7>
     </div>
   )
 }
--- src/WeatherService.js
+++ src/WeatherService.js
@@ -1,15 +1,15 @@
 
-
-const APIKey = bf2c20a1f20c38e0a1d11eb86ac5fe91
+import axios from 'axios'
+const APIKey = 'bf2c20a1f20c38e0a1d11eb86ac5fe91'
 const city = 'London'
 
 export const currentWeater = async () =>{
-     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`
      const currentWeather = await axios.get(URL).then(response => response.data)
      console.log(currentWeather)
 
      const {name, main:{humidity, temp, temp_max, temp_min}, sys:{country},wind:{speed}} = currentWeather
      
 
-     return {name, main:{humidity, temp, temp_max, temp_min}, sys:{country},wind:{speed}} 
+     return {name,humidity, temp, temp_max, temp_min,country,speed} 
 }
