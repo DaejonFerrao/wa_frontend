e4efc253f05e3620dfd1420072ac74922489a392
DaejonFerrao <daejonferrao@gmail.com>
2024-01-18 16:44:11 +0000

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
 import './App.css';
-
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import NavBar from './Components/Navbar/NavBar';
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,30 +1,42 @@
-import { useEffect , useState} from "react"
+import { useEffect, useState } from "react"
 import { currentWeater } from "../../WeatherService"
 
+import "./HomePage.css"
+
 export const HomePage = () => {
-const [currentWeatherState, setCurrentWeatherState] = useState({})
-    useEffect(()=>{
+  const [currentWeatherState, setCurrentWeatherState] = useState({})
+  useEffect(() => {
 
-        const handleCurrentWeather = async ()=>{
-            const weatherData = await currentWeater()
-             
-            setCurrentWeatherState(weatherData)
+    const handleCurrentWeather = async () => {
+      const weatherData = await currentWeater()
+
+      setCurrentWeatherState(weatherData)
 
 
-        }
-        handleCurrentWeather()
+    }
+    handleCurrentWeather()
 
-    },[])
+  }, [])
   return (
+
     <div>
-        <h1>Home page</h1>
-        <div>
-        <h2>{currentWeatherState.temp}</h2>
+      <h1>Home page</h1>
+      <div className="weather-data-group">
+
+      <div className="weather-location-group">
+        <h1>{currentWeatherState.country}</h1>
+        <h1>{currentWeatherState.name}</h1>
+      </div>
+
+      <div className="temp-group">
+        <span>Tempetature: </span><span>{currentWeatherState.temp}</span>
+      </div>
         <h3>{currentWeatherState.humidity}</h3>
         <h4>{currentWeatherState.speed}</h4>
-        <h5>{currentWeatherState.country}</h5>
+        
         <h6>{currentWeatherState.temp_min}</h6>
-        <h7>{currentWeatherState.name}</h7></div>
+        
+      </div>
     </div>
   )
 }
