f25c582139b1c6a10095d3d1e36a64bc89f6af5d
DaejonFerrao <daejonferrao@gmail.com>
2024-02-23 10:44:21 +0000

    made the text bolder

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -4,17 +4,17 @@ const APIKey = 'bf2c20a1f20c38e0a1d11eb86ac5fe91'
 const city = 'London'
 
 
-export const currentWeater = async (city) =>{
-     
+export const currentWeater = async (city) => {
+
      const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`
      const currentWeather = await axios.get(URL).then(response => response.data)
      // console.log(currentWeather)
 
-     const {name, main:{humidity, temp, temp_max, temp_min}, sys:{country},wind:{speed},coord:{lon,lat}} = currentWeather
-     
+     const { name, main: { humidity, temp, temp_max, temp_min }, sys: { country }, wind: { speed }, coord: { lon, lat } } = currentWeather
+
      // console.log(currentWeather)
 
-     return {name,humidity, temp, temp_max, temp_min,country,speed,lon,lat} 
+     return { name, humidity, temp, temp_max, temp_min, country, speed, lon, lat }
 }
 
 // export const airpollution = async () => {
@@ -25,13 +25,12 @@ export const getForecast = async (city) => {
      const URL_FORECAST = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=metric`
      const currentForecast = await axios.get(URL_FORECAST).then(response => response.data)
 
-     const {name, main:{humidity, temp, temp_max, temp_min}, sys:{country},wind:{speed},coord:{lon,lat}} = currentForecast
-     return {name,humidity, temp, temp_max, temp_min,country,speed,lon,lat} 
+     const { name, main: { humidity, temp, temp_max, temp_min }, sys: { country }, wind: { speed }, coord: { lon, lat } } = currentForecast
+     return { name, humidity, temp, temp_max, temp_min, country, speed, lon, lat }
 }
 
-     
-     //get the url same as above
-     // get the forecast
-     //log it to console
-     //return the 5 day forecast, preferably formatted but it might already be formatted.
 
+//get the url same as above
+// get the forecast
+//log it to console
+//return the 5 day forecast, preferably formatted but it might already be formatted.
